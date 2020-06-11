c12a41fc1d55639c2f8e08790b43cf9020aebe4f
Sybrand Aarnoutse <sybrand.s.y.b@gmail.com>
2020-06-11 22:10:25 +0200

    Refactor powermenu - Make source its own derivation - Allow specifying theme

--- default.nix
+++ default.nix
@@ -2,5 +2,9 @@
 
 rec {
 	rofi-unwrapped-git = pkgs.callPackage ./pkgs/rofi-unwrapped-git {};
-	powermenu = pkgs.callPackage ./pkgs/adi1090x/powermenu { inherit rofi-unwrapped-git; };
+        source-rofi = pkgs.callPackage ./pkgs/adi1090x/source-rofi {};
+        powermenu = pkgs.callPackage ./pkgs/adi1090x/powermenu {
+          inherit rofi-unwrapped-git;
+          inherit source-rofi;
+        };
 }
