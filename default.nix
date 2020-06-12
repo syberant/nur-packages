7c7abcecbd07522537a67cffb51f8f656319a99b
Sybrand Aarnoutse <sybrand.s.y.b@gmail.com>
2020-06-12 11:37:28 +0200

    Remove source-rofi from pkgs Refactor so dependant derivations won't break.

--- default.nix
+++ default.nix
@@ -2,13 +2,10 @@
 
 rec {
 	rofi-unwrapped-git = pkgs.callPackage ./pkgs/rofi-unwrapped-git {};
-        source-rofi = pkgs.callPackage ./pkgs/adi1090x/source-rofi {};
         powermenu = pkgs.callPackage ./pkgs/adi1090x/powermenu {
           inherit rofi-unwrapped-git;
-          inherit source-rofi;
         };
         launchers-git = pkgs.callPackage ./pkgs/adi1090x/launchers-git {
           inherit rofi-unwrapped-git;
-          inherit source-rofi;
         };
 }
